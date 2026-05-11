d7a63cbaa62818903418773d3b9f34167765981e
mangl-auf <kirillkorshnyak@mail.ru>
2026-05-11 19:55:42 +0500

    another try

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  //basePath: "/yaroslav-makarov1.github.io",
-  //assetPrefix: "/yaroslav-makarov1.github.io/",
-  assetPrefix: "./",
   typescript: {
     ignoreBuildErrors: true,
   },
