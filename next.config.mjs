e5261546b4e8991843e7e4b13fbdb68d81a77a75
mangl-auf <kirillkorshnyak@mail.ru>
2026-05-11 19:47:37 +0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/yaroslav-makarov1.github.io",
-  assetPrefix: "/yaroslav-makarov1.github.io/",
+  //basePath: "/yaroslav-makarov1.github.io",
+  //assetPrefix: "/yaroslav-makarov1.github.io/",
+  assetPrefix: "./",
   typescript: {
     ignoreBuildErrors: true,
   },
