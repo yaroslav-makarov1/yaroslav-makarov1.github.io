20f63b32a680fd3b2cc2dee94116007e03095010
mangl-auf <kirillkorshnyak@mail.ru>
2026-05-12 20:11:12 +0500

    minor changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Yaroslav | Student & Future Legend',
-  description: 'Personal website of Yaroslav - UrFU student, dreamer, and professional procrastinator',
+  title: 'Я - Ярослав Макаров',
+  description: 'Личный сайт Ярослава Макарова',
   generator: 'v0.app',
   icons: {
     icon: [
